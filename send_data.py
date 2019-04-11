4c7da4b5469b8232661a8079192e2353bd773f22
Noa Kurman <noa@bigpanda.io>
2019-04-11 16:15:59 +0300

    remove auth params

--- send_data.py
+++ send_data.py
@@ -6,17 +6,13 @@ import boto3
 import datetime
 
 host = 'search-kine-project-xgu3iblcsmpalo7cw3ksno7bie.us-east-2.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
-#url = 'https://search-kine-project-xgu3iblcsmpalo7cw3ksno7bie.us-east-2.es.amazonaws.com/test' # For example, my-test-domain.us-east-1.es.amazonaws.com
 region = 'us-east-2' # e.g. us-west-1
-secret_key = 'uj8O7MCNwPUnPfAipTb/DtAUAzjRer/R62DtiDKn'
-access_key = 'AKIAWJQCRR3BQZOAXNCQ'
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(access_key, secret_key, region, service)
 
 es = Elasticsearch(
     hosts = [{'host': host, 'port': 443}],
-#    http_auth = awsauth,
     use_ssl = True,
     verify_certs = True,
     connection_class = RequestsHttpConnection
@@ -37,7 +33,4 @@ document = {
 #    "year": "2020"
 #}
 
-#es.indices.create(index="test")
 es.index(index="test", doc_type="_doc", id="9", body=document)
-#r = requests.put(url, json=document)
-#print(es.get(index="test", doc_type="_doc", id="5"))
